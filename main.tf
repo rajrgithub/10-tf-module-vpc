c1d7e3d86442e42f6ccab5a0da32da8bfbd6a898
brajasekharreddy <raj479.mits@gmail.com>
2023-02-14 01:35:00 +0530

    display tag name with merge

--- main.tf
+++ main.tf
@@ -1,4 +1,13 @@
+#resource "aws_vpc" "main" {
+#  cidr_block = var.cidr_block
+#  tags = local.common_tags
+#}
+
+# To display tag name
 resource "aws_vpc" "main" {
   cidr_block = var.cidr_block
-  tags = local.common_tags
+  tags = merge(
+    local.common_tags,
+    { Name = "${var.env}-vpc" }
+  )
 }
