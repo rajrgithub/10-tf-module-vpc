7f59f925562f7048e90c6532a2e0a3ac2d34cc63
brajasekharreddy <raj479.mits@gmail.com>
2023-02-15 03:16:57 +0530

    Public and private subnets

--- main.tf
+++ main.tf
@@ -13,14 +13,36 @@ resource "aws_vpc" "main" {
 }
 
 # Subnets for dev-vpc
-resource "aws_subnet" "main" {
-  count      = length(var.subnets_cidr)
+#resource "aws_subnet" "main" {
+#  count      = length(var.subnets_cidr)
+#  vpc_id     = aws_vpc.main.id
+#  cidr_block = var.subnets_cidr[count.index]
+#
+#  tags = merge(
+#    local.common_tags,
+#    { Name = "${var.env}-subnet-${count.index + 1}" }
+#  )
+#}
+
+resource "aws_subnet" "public" {
+  count      = length(var.public_subnets_cidr)
   vpc_id     = aws_vpc.main.id
-  cidr_block = var.subnets_cidr[count.index]
+  cidr_block = var.public_subnets_cidr[count.index]
 
   tags = merge(
     local.common_tags,
-    { Name = "${var.env}-subnet-${count.index + 1}" }
+    { Name = "${var.env}-public-subnet-${count.index + 1}" }
+  )
+}
+
+resource "aws_subnet" "private" {
+  count      = length(var.private_subnets_cidr)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.private_subnets_cidr[count.index]
+
+  tags = merge(
+    local.common_tags,
+    { Name = "${var.env}-private-subnet-${count.index + 1}" }
   )
 }
 
@@ -36,26 +58,26 @@ resource "aws_vpc_peering_connection" "peer" {
   )
 }
 
-# Enable route form Dev VPC to Default VPC
-resource "aws_route" "default" {
-  route_table_id            = aws_vpc.main.default_route_table_id
-  destination_cidr_block    = "172.31.0.0/16"
-  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
-}
+## Enable route form Dev VPC to Default VPC
+#resource "aws_route" "default" {
+#  route_table_id            = aws_vpc.main.default_route_table_id
+#  destination_cidr_block    = "172.31.0.0/16"
+#  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+#}
+#
+## for internet access
+#resource "aws_route" "igw-route" {
+#  route_table_id            = aws_vpc.main.default_route_table_id
+#  destination_cidr_block    = "0.0.0.0/0"
+#  gateway_id = aws_internet_gateway.igw.id
+#}
 
-# for internet access
-resource "aws_route" "igw-route" {
-  route_table_id            = aws_vpc.main.default_route_table_id
-  destination_cidr_block    = "0.0.0.0/0"
-  gateway_id = aws_internet_gateway.igw.id
-}
-
-# Enable route form Default VPC to Dev VPC
-resource "aws_route" "default-vpc" {
-  route_table_id            = data.aws_vpc.default.main_route_table_id
-  destination_cidr_block    = var.cidr_block
-  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
-}
+## Enable route form Default VPC to Dev VPC
+#resource "aws_route" "default-vpc" {
+#  route_table_id            = data.aws_vpc.default.main_route_table_id
+#  destination_cidr_block    = var.cidr_block
+#  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+#}
 
 
 # Internet Gateway to dev vpc instance
@@ -71,45 +93,45 @@ resource "aws_internet_gateway" "igw" {
 
 
 // Create EC2 instance
-data "aws_ami" "centos8" {
-  most_recent = true
-  name_regex  = "Centos-8-DevOps-Practice"
-  owners      = ["973714476881"]
-}
-
-resource "aws_instance" "web" {
-  ami                    = data.aws_ami.centos8.id
-  instance_type          = "t3.micro"
-  vpc_security_group_ids = [aws_security_group.allow_tls.id]
-  subnet_id              = aws_subnet.main.*.id[0]
-
-  tags = {
-    Name = "test-centos8"
-  }
-}
-
-resource "aws_security_group" "allow_tls" {
-  name        = "allow_tls"
-  description = "Allow TLS inbound traffic"
-  vpc_id      = aws_vpc.main.id
-
-  ingress {
-    description = "TLS from VPC"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  tags = {
-    Name = "allow_tls"
-  }
-}
+#data "aws_ami" "centos8" {
+#  most_recent = true
+#  name_regex  = "Centos-8-DevOps-Practice"
+#  owners      = ["973714476881"]
+#}
+#
+#resource "aws_instance" "web" {
+#  ami                    = data.aws_ami.centos8.id
+#  instance_type          = "t3.micro"
+#  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+#  subnet_id              = aws_subnet.main.*.id[0]
+#
+#  tags = {
+#    Name = "test-centos8"
+#  }
+#}
+#
+#resource "aws_security_group" "allow_tls" {
+#  name        = "allow_tls"
+#  description = "Allow TLS inbound traffic"
+#  vpc_id      = aws_vpc.main.id
+#
+#  ingress {
+#    description = "TLS from VPC"
+#    from_port   = 22
+#    to_port     = 22
+#    protocol    = "tcp"
+#    cidr_blocks = ["0.0.0.0/0"]
+#  }
+#
+#  egress {
+#    from_port        = 0
+#    to_port          = 0
+#    protocol         = "-1"
+#    cidr_blocks      = ["0.0.0.0/0"]
+#    ipv6_cidr_blocks = ["::/0"]
+#  }
+#
+#  tags = {
+#    Name = "allow_tls"
+#  }
+#}
--- vars.tf
+++ vars.tf
@@ -1,4 +1,6 @@
 variable "cidr_block"{}
 variable "env"{}
-variable "subnets_cidr" {}
+#variable "subnets_cidr" {}
+variable "public_subnets_cidr" {}
+variable "private_subnets_cidr" {}
 variable "default_vpc_id" {}
