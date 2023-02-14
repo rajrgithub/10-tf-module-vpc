312ff48c4c390d55d448d2571f4045591f1c982d
brajasekharreddy <raj479.mits@gmail.com>
2023-02-14 23:17:33 +0530

    enable route from default VPC to created VPC(dev-VPC)

--- main.tf
+++ main.tf
@@ -12,6 +12,7 @@ resource "aws_vpc" "main" {
   )
 }
 
+# Subnets for dev-vpc
 resource "aws_subnet" "main" {
   count      = length(var.subnets_cidr)
   vpc_id     = aws_vpc.main.id
@@ -23,6 +24,7 @@ resource "aws_subnet" "main" {
   )
 }
 
+# Peering connection between Default VPC and Dev VPC
 resource "aws_vpc_peering_connection" "peer" {
   peer_owner_id = data.aws_caller_identity.current.account_id
   peer_vpc_id   = var.default_vpc_id
@@ -34,13 +36,14 @@ resource "aws_vpc_peering_connection" "peer" {
   )
 }
 
-
+# Enable route form Dev VPC to Default VPC
 resource "aws_route" "default" {
   route_table_id            = aws_vpc.main.default_route_table_id
   destination_cidr_block    = "172.31.0.0/16"
   vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
 }
 
+# Enable route form Default VPC to Dev VPC
 resource "aws_route" "default-vpc" {
   route_table_id            = data.aws_vpc.default.main_route_table_id
   destination_cidr_block    = var.cidr_block
