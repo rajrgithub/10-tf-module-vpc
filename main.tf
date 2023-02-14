167aab306c6c6c3188c49c28f2f62e9f26cce21b
brajasekharreddy <raj479.mits@gmail.com>
2023-02-14 23:12:25 +0530

    enable route from default VPC to created VPC(dev-VPC)

--- main.tf
+++ main.tf
@@ -41,3 +41,8 @@ resource "aws_route" "default" {
   vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
 }
 
+resource "aws_route" "default-vpc" {
+  route_table_id            = data.aws_vpc.default.main_route_table_id
+  destination_cidr_block    = var.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+}
