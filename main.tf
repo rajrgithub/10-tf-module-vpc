3e48d93f6ca9665805193a8c384601bf869e3444
brajasekharreddy <raj479.mits@gmail.com>
2023-02-15 03:47:27 +0530

    Public and private subnets

--- main.tf
+++ main.tf
@@ -90,48 +90,121 @@ resource "aws_internet_gateway" "igw" {
   )
 }
 
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  route {
+    cidr_block                = data.aws_vpc.default.cidr_block
+    vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+  }
+
+  tags = merge(
+    local.common_tags,
+    { Name = "${var.env}-public-route-table" }
+  )
+}
+
+resource "aws_route_table_association" "public-rt-assoc" {
+  count          = length(aws_subnet.public)
+  subnet_id      = aws_subnet.public.*.id[count.index]
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_eip" "ngw-eip" {
+  vpc = true
+}
+
+resource "aws_nat_gateway" "ngw" {
+  allocation_id = aws_eip.ngw-eip.id
+  subnet_id     = aws_subnet.public.*.id[0]
+
+  tags = merge(
+    local.common_tags,
+    { Name = "${var.env}-ngw" }
+  )
+
+  //depends_on = [aws_internet_gateway.example]
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block                = data.aws_vpc.default.cidr_block
+    vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+  }
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.ngw.id
+  }
+
+  tags = merge(
+    local.common_tags,
+    { Name = "${var.env}-private-route-table" }
+  )
+}
+
+resource "aws_route_table_association" "private-rt-assoc" {
+  count          = length(aws_subnet.private)
+  subnet_id      = aws_subnet.private.*.id[count.index]
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route" "r" {
+  route_table_id            = data.aws_vpc.default.main_route_table_id
+  destination_cidr_block    = var.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+}
+
 
 
 // Create EC2 instance
-#data "aws_ami" "centos8" {
-#  most_recent = true
-#  name_regex  = "Centos-8-DevOps-Practice"
-#  owners      = ["973714476881"]
-#}
-#
-#resource "aws_instance" "web" {
-#  ami                    = data.aws_ami.centos8.id
-#  instance_type          = "t3.micro"
-#  vpc_security_group_ids = [aws_security_group.allow_tls.id]
-#  subnet_id              = aws_subnet.main.*.id[0]
-#
-#  tags = {
-#    Name = "test-centos8"
-#  }
-#}
-#
-#resource "aws_security_group" "allow_tls" {
-#  name        = "allow_tls"
-#  description = "Allow TLS inbound traffic"
-#  vpc_id      = aws_vpc.main.id
-#
-#  ingress {
-#    description = "TLS from VPC"
-#    from_port   = 22
-#    to_port     = 22
-#    protocol    = "tcp"
-#    cidr_blocks = ["0.0.0.0/0"]
-#  }
-#
-#  egress {
-#    from_port        = 0
-#    to_port          = 0
-#    protocol         = "-1"
-#    cidr_blocks      = ["0.0.0.0/0"]
-#    ipv6_cidr_blocks = ["::/0"]
-#  }
-#
-#  tags = {
-#    Name = "allow_tls"
-#  }
-#}
+data "aws_ami" "centos8" {
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = ["973714476881"]
+}
+
+resource "aws_instance" "web" {
+  ami                    = data.aws_ami.centos8.id
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  #subnet_id              = aws_subnet.main.*.id[0]
+  subnet_id              = aws_subnet.private.*.id[0]
+
+  tags = {
+    Name = "test-centos8"
+  }
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
