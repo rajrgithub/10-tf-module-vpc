980a2c4ba7f73f6b46a84ebb45c253f6e8299e5c
brajasekharreddy <raj479.mits@gmail.com>
2023-02-15 00:02:14 +0530

    Create Ec2 instance

--- main.tf
+++ main.tf
@@ -60,3 +60,49 @@ resource "aws_internet_gateway" "igw" {
     { Name = "${var.env}-igw" }
   )
 }
+
+
+
+// Create EC2 instance
+data "aws_ami" "centos8" {
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = ["973714476881"]
+}
+
+resource "aws_instance" "web" {
+  ami                    = data.aws_ami.centos8.id
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  subnet_id              = aws_subnet.private.*.id[0]
+
+  tags = {
+    Name = "test-centos8"
+  }
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
